722a72c813c4ee691fc89abb5e595dabdcc6e839
Nic McPhee <nic.mcphee@gmail.com>
2010-06-18 21:11:21 -0500

    Added workling as a plugin (script/plugin install http://svn.playtype.net/plugins/workling/); this added a number of config and script files.  All my cucumber tests failed at first until I added a cucumber entry to config/workling.yml; very annoying and took a while to debug.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,3 +15,6 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# Start up instances of Workling and Starling
+Workling::Remote.dispatcher = Workling::Remote::Runners::StarlingRunner.new
